d3c57bdf3b0bd65cbd98daf1c0fd846b5181a8af
Giuliano Accorsi <giulianoaccorsi@gmail.com>
2022-08-09 21:40:55 -0300

    Corrige Exercicio

---
+++ app/src/main/java/com/giuliano/sharedpreferences/Saudacao.kt
@@ -0,0 +1,29 @@
+package com.giuliano.sharedpreferences
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.giuliano.sharedpreferences.databinding.ActivityMainBinding
+import com.giuliano.sharedpreferences.databinding.ActivitySaudacaoBinding
+
+class Saudacao : AppCompatActivity() {
+    private val binding: ActivitySaudacaoBinding by lazy {
+        ActivitySaudacaoBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saudacao)
+
+        val saudacaoPersistencia = this.getSharedPreferences("saudacao", Context.MODE_PRIVATE)
+
+        val nome = saudacaoPersistencia.getString("nome","")
+        val tratamento = saudacaoPersistencia.getString("tratamento", "")
+
+        if(tratamento.equals("Sem Tratamento")) {
+            binding.lbSaudacao.text = nome
+        }else {
+            binding.lbSaudacao.text = tratamento + " " + nome
+        }
+    }
+}
